33b24f5e8ef80fd112600accc1eec6ac7b520b8b
ServaneBeaudier <servanebeaudier@gmail.com>
2025-05-19 13:25:18 +0200

    first commit

---
+++ src/main/java/fr/gourmetgo/ProjetGourmetgoApplication.java
@@ -0,0 +1,13 @@
+package fr.gourmetgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGourmetgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGourmetgoApplication.class, args);
+	}
+
+}
